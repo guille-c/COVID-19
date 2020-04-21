3289ecbf82700ff0f2f0092be7946ce18fe2f35c
Guille <guillecabrera@udec.cl>
2020-04-21 17:53:17 -0400

    Add notebook for Chillan and intermitent quarantines

--- src/SEIR_exams.py
+++ src/SEIR_exams.py
@@ -9,7 +9,6 @@ def sigmoid(x, x0, k, b = 0., L = 1.):
 def SEIR_exams (t, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date, k, 
                 a = 0., Imax = np.inf):
     num_steps = len(t) - 1
-    alphas_C = 1 + (a-1.)/(1+np.exp(-k*(t-a_date)))
     
     S = np.zeros(num_steps + 1)
     E = np.zeros(num_steps + 1)
@@ -32,15 +31,18 @@ def SEIR_exams (t, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date, k,
         E[step+1] = E[step] + (beta*I[step]*S[step] - sigma*E[step])*dt
         I[step+1] = I[step] + (sigma*E[step] - gamma*I[step])*dt
         R[step+1] = R[step] + gamma*I[step]*dt
-        C[step+1] = C[step] + sigma*E[step]
-        C_m[step + 1] = C_m[step] + alphas_C[step]*sigma*E[step]  
+        C[step+1] = C[step] + sigma*E[step]*dt
+
+    dCdt = sigma*E
+    alphas_C = 1 + (a-1.)/(1+np.exp(-k*(dCdt-a_date)))
+    for step in range(num_steps):
+        C_m[step + 1] = C_m[step] + alphas_C[step]*sigma*E[step]*dt
         #I_m[step+1] = I_m[step] + alpha * sigma * E[step]
     return S, E, I, R, C, C_m
 
 def SEIR_exams_backward (t, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date, k, 
                          a = 0., Imax = np.inf):
     num_steps = len(t) - 1
-    alphas_C = 1 + (a-1.)/(1+np.exp(-k*(t-a_date)))
     
     S = np.zeros(num_steps + 1)
     E = np.zeros(num_steps + 1)
@@ -58,7 +60,6 @@ def SEIR_exams_backward (t, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date,
 
     for step in range(num_steps):
         dt = t[step+1]-t[step]
-        dt = t[step+1]-t[step]
         a_ = E[step] + S[step]
         b_ = 1 + dt * sigma
         c_ = dt*beta/(1+dt*gamma)
@@ -70,8 +71,13 @@ def SEIR_exams_backward (t, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date,
         I[step + 1] = (I[step] + dt*sigma*E[step+1])/(1.+dt*gamma)
         S[step + 1] = S[step]/(1. + dt*beta*I[step+1]) 
         R[step + 1] = R[step] + gamma*I[step + 1]*dt
-        C[step + 1] = C[step] + sigma*E[step + 1]
-        C_m[step + 1] = C_m[step] + alphas_C[step]*sigma*E[step + 1]  
+        C[step + 1] = C[step] + sigma*E[step + 1]*dt
+ 
+    dCdt = sigma*E
+    alphas_C = 1 + (a-1.)/(1+np.exp(-k*(dCdt-a_date)))
+    for step in range(num_steps):
+        dt = t[step+1]-t[step]
+        C_m[step + 1] = C_m[step] + alphas_C[step+1]*sigma*E[step + 1]*dt
         #I_m[step+1] = I_m[step] + alpha * sigma * E[step]
         
     return S, E, I, R, C, C_m
@@ -88,7 +94,7 @@ def GridSearchSEIR_exams (ts, s0, e0, i0, r0, c0, c0m, C_real,
                           infectious_time = np.arange(5., 22, 1.), # days
                           ks = 10**np.linspace(-3, -1, 9),
                           a_dates = np.arange (5, 41, 5),
-                          a_s = np.linspace(0.25, 0.75, 9), val_R = False):
+                          a_s = np.linspace(0.25, 0.75, 9), val_R = False, backward = True):
     
     #print (len(ts), len(I_real))
     #transmission_coeff = np.power(10, np.arange(-6, -10)) # 1 / day person
@@ -115,7 +121,11 @@ def GridSearchSEIR_exams (ts, s0, e0, i0, r0, c0, c0m, C_real,
                 for k in ks:
                     for a in a_s:
                         for a_date in a_dates:
-                            S, E, I, R, C, Cm = SEIR_exams (ts, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date, k, a)
+                            if backward:
+                                S, E, I, R, C, Cm = SEIR_exams_backward (ts, s0, e0, i0, r0, c0, c0m, 
+                                                                         beta, sigma, gamma, a_date, k, a)
+                            else:
+                                S, E, I, R, C, Cm = SEIR_exams (ts, s0, e0, i0, r0, c0, c0m, beta, sigma, gamma, a_date, k, a)
                             if val_R:
                                 RMSE = ValidateSEIR_exams_IR (I_real, R_real, Im, Rm)
                             else:
